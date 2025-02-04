f4c2f439a17d9aceefde0fa14efb1181541dbd4d
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-02-05 00:46:04 +0100

    modification

--- stage1/wsgi.py
+++ stage1/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from waitress import serve
-from stage1.wsgi import application
+from stage1 import wsgi  # Import from stage1 directly instead of stage1.wsgi
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stage1.settings")
-    serve(application, host="0.0.0.0", port=8000)
-
+    serve(wsgi.application, host="0.0.0.0", port=8000)  # Use wsgi.application here
